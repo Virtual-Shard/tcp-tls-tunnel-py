c3ab08460fc19c69e36c73b611f0e9c4fd5382de
David Ikkes <david.ikkes@chisw-dev.com>
2020-11-07 20:52:05 +0200

    update setup file for deploying into pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
-from setuptools import setup
+import pathlib
+from setuptools import setup, find_packages
+
+BASE_DIR = pathlib.Path(__file__).parent
+README = (BASE_DIR / "README.md").read_text()
+
 
 setup(
     name="tls-tunnel",
-    version='0.1'
+    version='0.1',
+    description="TLS TCP tunnel for HTTP requests",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Arteha/tcp-tls-tunnel-py",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    packages=find_packages(exclude=("tests",)),
 )
